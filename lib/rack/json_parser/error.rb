a5a9bc1c697f09e6e24aefb15b2ab642a1a1afa7
Joakim Reinert <mail@jreinert.com>
2017-02-02 19:42:55 +0100

    fix rubocop offenses

--- lib/rack/json_parser/error.rb
+++ lib/rack/json_parser/error.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Rack
   class JSONParser
     # Base class for all JSONParser errors
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'bundler/setup'
 require 'simplecov'
 require 'rack/json_parser'
